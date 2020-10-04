6e39e94917c2e77a4f481a80fb7ffa0d7ba2266b
Filip Cwiek <fifek.inex@gmail.com>
2020-10-04 12:14:08 +0200

    Change in models

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  author: { type: String, required: true },
-  created: { type: Date, required: true },
-  updated: { type: Date, required: true },
+  email: { type: String, required: true },
+  dateOfPublication: { type: Date, required: true },
+  dateOfUpdate: { type: Date, required: true },
   status: { type: String, required: true },
   title: { type: String, required: true },
-  text: { type: String, required: true },
-  photo: { type: String },
+  description: { type: String, required: true },
   price: { type: Number },
-  phone: { type: String },
-  location: { type: String },
 });
 
 module.exports = mongoose.model('Post', postSchema);
